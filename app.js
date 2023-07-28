261167fb65b7bcee32cf33c9175e21f4065624ab
pitertan <pitertan0120@gmail.com>
2023-07-28 19:08:54 +0700

    modify app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
+const commentedPosts = new Set();
 
 autoComment('https://www.facebook.com/');
 
@@ -8,64 +9,94 @@ function delay(ms) {
 }
 
 async function autoComment(url) {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-            "--no-sandbox",
-            "--disable-gpu",
-        ],
-        channel: "chrome"
-    });
-
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(url, {
-        timeout: 0,
-        waitUntil: 'networkidle0',
-    });
-
-    //   wait
-    await page.waitForSelector('input#email');
-    await page.type('input#email', 'justinlaurenso0@gmail.com', { delay: 100 });
-    await page.waitForSelector('input#pass');
-    await page.type('input#pass', 'testing123', { delay: 100 });
-    await page.keyboard.press('Enter');
-
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
-
-    await page.goto("https://www.facebook.com/groups/106641399183172/", {
-        timeout: 0,
-        waitUntil: 'networkidle0',
-    });
-
-    while (true) {
-        // Wait for the new post to appear
-        await page.waitForSelector('div[role="article"]');
-
-        // Check if the newest post has an image
-        const latestPostImage = await page.evaluate(() => {
-            const postElement = document.querySelector('div[role="article"]');
-            const imageElement = postElement.querySelector('img');
-            return imageElement ? imageElement.src : null;
+    try {
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                "--no-sandbox",
+                "--disable-gpu",
+            ],
+            channel: "chrome"
         });
 
-        if (latestPostImage) {
-            // If the newest post has an image, comment on it
-            const comment = "This is an automated comment using Puppeteer!";
-            await page.waitForSelector('div[role="textbox"]');
-            await page.type('div[role="textbox"]', comment, { delay: 100 });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(url, {
+            timeout: 0,
+            waitUntil: 'networkidle0',
+        });
 
-            // Press Enter to post the comment
-            await page.keyboard.press('Enter');
+        // Login process
+        await page.waitForSelector('input#email');
+        await page.type('input#email', 'pitertan0120@gmail.com', { delay: 100 });
+        await page.waitForSelector('input#pass');
+        await page.type('input#pass', 'pitertan2001', { delay: 100 });
+        await page.keyboard.press('Enter');
 
-            // Wait for a short time to ensure the comment is posted successfully
-            await delay(2000);
-            console.log("Comment posted successfully");
-            break; // Exit the loop after posting the comment
-        } else {
-            // If the newest post doesn't have an image, refresh the page
-            await page.reload({ waitUntil: 'networkidle0' });
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+        await page.goto("https://www.facebook.com/groups/106641399183172/", {
+            timeout: 0,
+            waitUntil: 'networkidle0',
+        });
+
+        while (true) {
+            // Wait for the new post to appear
+            await page.waitForSelector('div[role="article"]');
+
+            // Check if the newest post has an image
+            const latestPostImage = await page.evaluate(() => {
+                const postElement = document.querySelector('div[role="article"]');
+                const imageElement = postElement.querySelector('img');
+                return imageElement ? imageElement.src : null;
+            });
+
+            if (latestPostImage) {
+                // If the newest post has an image and hasn't been commented yet, comment on it
+                if (!commentedPosts.has(latestPostImage)) {
+                    // Get the list of comments in the post
+                    const comments = await page.evaluate(() => {
+                        const commentElements = document.querySelectorAll('div[data-testid="comment"]');
+                        const commentsArray = [];
+                        commentElements.forEach((commentElement) => {
+                            const commentText = commentElement.querySelector('span[dir="ltr"]').textContent.trim();
+                            commentsArray.push(commentText);
+                        });
+                        return commentsArray;
+                    });
+
+                    const commentToPost = "Ini adalah komentar otomatis menggunakan Puppeteer!";
+                    const isCommentExist = comments.some((comment) => comment === commentToPost);
+
+                    if (!isCommentExist) {
+                        // If the comment doesn't exist in the post, proceed to post the comment
+                        await page.waitForSelector('div[role="textbox"]');
+                        await page.type('div[role="textbox"]', commentToPost, { delay: 100 });
+                        await page.keyboard.press('Enter');
+
+                        // Add the post URL to the set of commentedPosts
+                        commentedPosts.add(latestPostImage);
+
+                        // Wait for a short time to ensure the comment is posted successfully
+                        await delay(2000);
+                        console.log("Komentar berhasil diposting");
+                    } else {
+                        console.log("Komentar yang sama sudah ada dalam postingan. Melewati...");
+                    }
+                } else {
+                    console.log("Postingan sudah dikomentari. Melewati...");
+                }
+
+                // Refresh the page regardless of whether the comment was posted or skipped
+                await page.reload({ waitUntil: 'networkidle0' });
+                console.log("Refreshing halaman...");
+            } else {
+                // If the newest post doesn't have an image, exit the loop
+                await page.reload({ waitUntil: 'networkidle0' });
             console.log("No image found. Refreshing the page...");
+            }
         }
+    } catch (error) {
+        console.error("Terjadi kesalahan:", error);
     }
 }
