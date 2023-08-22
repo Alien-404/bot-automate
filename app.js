9542215a2389fa2baaa28ed4932ac0bc594e87ca
Justin Laurenso <justin.laurenso166@gmail.com>
2023-08-22 07:44:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // modules
-const puppeteer = require("puppeteer-core");
-const configAccount = require("./config.json");
-const { spawn } = require("child_process");
-const commentedFile = require("./commented.json");
-const fs = require("fs");
-const axios = require("axios");
-const bcrypt = require("bcrypt");
-const jsdom = require("jsdom");
+const puppeteer = require('puppeteer-core');
+const configAccount = require('./config.json');
+const { spawn } = require('child_process');
+const commentedFile = require('./commented.json');
+const fs = require('fs');
+const axios = require('axios');
+const bcrypt = require('bcrypt');
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 // config
@@ -21,13 +21,13 @@ function delay(ms) {
 async function writeCommentedPosts() {
   try {
     await fs.promises.writeFile(
-      "./commented.json",
+      './commented.json',
       JSON.stringify([...commentedPosts]),
-      "utf8"
+      'utf8'
     );
   } catch (error) {
     console.error(
-      "Terjadi kesalahan saat menulis ke commentedPosts.json:",
+      'Terjadi kesalahan saat menulis ke commentedPosts.json:',
       error
     );
   }
@@ -41,11 +41,8 @@ function compareUrlsUntilJpg(url1, url2) {
   const path2 = urlObj2.pathname;
 
   // Compare the paths until .jpg
-  if (path1.includes(".jpg") && path2.includes(".jpg")) {
-    return (
-      path1.substring(0, path1.indexOf(".jpg")) ===
-      path2.substring(0, path2.indexOf(".jpg"))
-    );
+  if (path1.includes('.jpg') && path2.includes('.jpg')) {
+    return path1.substring(0, path1.indexOf('.jpg')) === path2.substring(0, path2.indexOf('.jpg'));
   }
 
   return false;
@@ -64,24 +61,22 @@ function formatGroup(url) {
 }
 
 async function extractItems() {
-  const feedElements = document.querySelectorAll(
-    'div[role="feed"] > .x1yztbdb'
-  );
+  const feedElements = document.querySelectorAll('div[role="feed"] > .x1yztbdb');
   const newPosts = [];
 
   // const numToPosts = Math.min(feedElements.length, 100)
 
   for (let i = 0; i < 100; i++) {
     if (feedElements[i]) {
-      const checkImage =
-        feedElements && feedElements[i].querySelector("div:nth-child(8)");
+
+      const checkImage = feedElements && feedElements[i].querySelector('div:nth-child(8)');
 
       // if (checkImage && checkAdmin) {
       if (checkImage) {
         const checkAdmin = feedElements[i].querySelector(".x1j85h84");
-        if (checkAdmin && checkAdmin.innerHTML === "Admin") {
-          const imageElement = checkImage.querySelector("img.x1ey2m1c");
-          const result = imageElement ? imageElement.src : null;
+        if (checkAdmin && checkAdmin.innerHTML === 'Admin') {
+          const imageElement = checkImage.querySelector('img.x1ey2m1c');
+          const result = imageElement ? imageElement.src : null
           if (result !== null) {
             feedElements[i].classList.add(`item-0`);
             newPosts.push(result);
@@ -95,23 +90,27 @@ async function extractItems() {
   return newPosts;
 }
 
-async function scrapeItems(page, extractItems, itemCount, scrollDelay = 100) {
+
+async function scrapeItems(
+  page,
+  extractItems,
+  itemCount,
+  scrollDelay = 100,
+) {
   let items = [];
   try {
     let previousHeight;
     while (items.length < itemCount) {
       items = await page.evaluate(extractItems);
-      previousHeight = await page.evaluate("document.body.scrollHeight");
-      await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
-      await page.waitForFunction(
-        `document.body.scrollHeight > ${previousHeight}`
-      );
+      previousHeight = await page.evaluate('document.body.scrollHeight');
+      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
       await page.waitForTimeout(scrollDelay);
       if (items.length > 0) {
         break;
       }
     }
-  } catch (e) {}
+  } catch (e) { }
   return items;
 }
 
@@ -119,48 +118,45 @@ async function scrapeItems(page, extractItems, itemCount, scrollDelay = 100) {
 async function mainBot() {
   try {
     // config puppeteer | just ignore it
-    console.log("Launch browser...");
+    console.log('Launch browser...');
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-gpu", "--disable-notifications"],
-      channel: "chrome",
-      executablePath: "/usr/bin/chromium-browser",
+      args: ['--no-sandbox', '--disable-gpu', "--disable-notifications"],
+      channel: 'chrome',
+      executablePath: '/usr/bin/chromium-browser',
     });
 
     // setup browser | just ignore it
-    console.log("Go to facebook...");
+    console.log('Go to facebook...');
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
     await page.setViewport({ width: 1920, height: 1080 });
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
-    );
-    await page.goto("https://web.facebook.com/", {
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36');
+    await page.goto('https://web.facebook.com/', {
       timeout: 0,
-      waitUntil: "networkidle2",
+      waitUntil: 'networkidle2',
     });
 
     // login process
-    await page.waitForSelector("input#email");
-    await page.type("input#email", configAccount.account.email, { delay: 0 });
-    await page.waitForSelector("input#pass");
-    await page.type("input#pass", configAccount.account.password, {
+    await page.waitForSelector('input#email');
+    await page.type('input#email', configAccount.account.email, { delay: 0 });
+    await page.waitForSelector('input#pass');
+    await page.type('input#pass', configAccount.account.password, {
       delay: 0,
     });
-    await page.keyboard.press("Enter");
+    await page.keyboard.press('Enter');
 
     // waiting page load
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    console.log("Login success...");
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    console.log('Login success...');
 
-    await page.goto(
-      formatGroup(configAccount.group_url) + "?sorting_setting=CHRONOLOGICAL",
+    await page.goto(formatGroup(configAccount.group_url) + '?sorting_setting=CHRONOLOGICAL',
       {
         timeout: 0,
-        waitUntil: "networkidle2",
+        waitUntil: 'networkidle2',
       }
     );
-    console.log("Go to group url : ", formatGroup(configAccount.group_url));
+    console.log('Go to group url : ', formatGroup(configAccount.group_url));
 
     // infinite loop
     // while (true) {
@@ -169,65 +165,57 @@ async function mainBot() {
     while (true) {
       const feedVisible = await page.evaluate(() => {
         const feed = document.querySelector('div[role="feed"]');
-        return feed && window.getComputedStyle(feed).display !== "none";
+        return feed && window.getComputedStyle(feed).display !== 'none';
       });
 
       if (feedVisible) {
-        break; // Exit the loop if the feed is visible
+        break; // Exit the loop if the feed is visible  
       }
 
-      await page.reload({ waitUntil: "networkidle2" });
+      await page.reload({ waitUntil: 'networkidle2' });
       await delay(3000); // Wait for 3 second before checking again
     }
 
     while (true) {
-      console.log("Checking post...");
+      console.log("Checking post...")
       const items = await scrapeItems(page, extractItems, 100);
 
       let textBoxs = [];
       for (const item of items) {
-        const found = await page.evaluate(
-          (item) => {
-            const feedElements = document.querySelectorAll(
-              'div[role="feed"] > .x1yztbdb'
-            );
+        const found = await page.evaluate((item) => {
+          const feedElements = document.querySelectorAll('div[role="feed"] > .x1yztbdb');
 
-            for (let i = 0; i < feedElements.length; i++) {
-              if (feedElements[i]) {
-                const imageSelector = `img.x1ey2m1c[src="${item}"]`;
-                const foundImage = feedElements[i].querySelector(imageSelector);
+          for (let i = 0; i < feedElements.length; i++) {
+            if (feedElements[i]) {
+              const imageSelector = `img.x1ey2m1c[src="${item}"]`;
+              const foundImage = feedElements[i].querySelector(imageSelector);
 
-                if (foundImage) {
-                  let commentBox = feedElements[i].querySelector(
-                    "div[role='textbox']"
-                  );
-                  if (commentBox) {
-                    return feedElements[i].innerHTML;
-                  } else {
-                    return null;
-                  }
+              if (foundImage) {
+                let commentBox = feedElements[i].querySelector("div[role='textbox']");
+                if (commentBox) {
+                  return feedElements[i].innerHTML;
+                } else {
+                  return null
                 }
               }
             }
+          }
 
-            return null;
-          },
-          item,
-          configAccount.comment
-        );
+          return null;
+        }, item, configAccount.comment);
 
         textBoxs.push(found);
       }
 
       // run function to comment on the post
       await commentOnPosts(page, textBoxs).then(async () => {
-        console.log("Refreshing...");
+        console.log("Refreshing...")
         await delay(1000);
-        await page.reload({ waitUntil: "networkidle2" });
+        await page.reload({ waitUntil: 'networkidle2' })
       });
     }
   } catch (error) {
-    console.error("something error : ", error);
+    console.error('something error : ', error);
   }
 }
 
@@ -242,22 +230,15 @@ async function commentOnPosts(page, textBoxs) {
       const dom = new JSDOM(textBoxString);
       const document = dom.window.document;
 
-      const imgElements = document.querySelectorAll("img.x1ey2m1c");
-      let imgSource = imgElements[0].getAttribute("src");
+      const imgElements = document.querySelectorAll('img.x1ey2m1c');
+      let imgSource = imgElements[0].getAttribute('src');
 
       // check img src
       if (imgSource) {
         // jika post belum pernah di komentari
-        if (
-          ![...commentedPosts].some((item) =>
-            compareUrlsUntilJpg(item, imgSource)
-          )
-        ) {
+        if (![...commentedPosts].some(item => compareUrlsUntilJpg(item, imgSource))) {
           const commentPromise = (async () => {
-            await page.type(
-              `.item-${i} div[role='textbox']`,
-              configAccount.comment
-            );
+            await page.type(`.item-${i} div[role='textbox']`, configAccount.comment);
             await page.keyboard.press("Enter");
 
             commentedPosts.add(imgSource);
@@ -267,10 +248,11 @@ async function commentOnPosts(page, textBoxs) {
 
           commentPromises.push(commentPromise);
         } else {
-          console.log("Postingan sudah dikomentari. Melewati...");
+          console.log('Postingan sudah dikomentari. Melewati...');
         }
       }
-    } else {
+    }
+    else {
       console.log("Tidak menemukan textbox untuk dikomentari");
     }
     // await delay(1000);
@@ -288,14 +270,14 @@ async function commentOnPosts(page, textBoxs) {
   ) {
     console.log(`please setup the account`);
     // Run the second script using child_process.spawn
-    spawn("node", ["config/config.js"], {
-      stdio: "inherit",
+    spawn('node', ['config/config.js'], {
+      stdio: 'inherit',
     });
   } else {
     // get key from api
     try {
       const response = await axios.get(
-        "https://api-key-bot.vercel.app/api/key"
+        'https://api-key-bot.vercel.app/api/key'
       );
       const data = await response.data;
 
@@ -303,12 +285,12 @@ async function commentOnPosts(page, textBoxs) {
       const isValid = await bcrypt.compare(key, data.key);
 
       if (!isValid) {
-        throw new Error("kode akses salah!");
+        throw new Error('kode akses salah!');
       }
 
       mainBot();
     } catch (error) {
-      console.log("error: ", error.message);
+      console.log('error: ', error.message);
     }
   }
 })();
